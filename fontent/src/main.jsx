ba47081f90d1cf0c0c7e84deb16059f750758fa4
ADITYAPRASADSAHOOS1200 <apsahoo5555@gmail.com>
2024-02-16 02:52:16 +0530

    working on login page

--- fontent/src/main.jsx
+++ fontent/src/main.jsx
@@ -3,10 +3,14 @@ import './index.css'
 import ReactDOM from 'react-dom/client'
 import Header from './Pages/Home/Header/Header'
 import Landing from './Pages/Home/Landing/Landing'
+import LeftSec from './Pages/Login/leftSec'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
-    <Landing/>
+    <Landing/> 
+    <LeftSec/>
   </React.StrictMode>,
 )
