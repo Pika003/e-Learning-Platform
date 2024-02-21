fcf7cebfd3fb7efd0cd725b50848f2d9b001d248
Pika003 <satyajitmaity0123.com>
2024-02-21 21:26:01 +0530

    some chnages in login and sign up

--- frontend/src/Pages/Home/About/About.jsx
+++ frontend/src/Pages/Home/About/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>About</div>
+    <div id='about'>
+      <div><h2>About Us</h2></div>
+      <div>
+        <div><img src="" alt="" /></div>
+        <div><p>Welcome to our platform! We are a team of final year students pursuing our Master's in Computer Applications (MCA) with a shared passion for education and technology. Driven by our commitment to facilitate learning and academic growth, we have created this website as a bridge between proficient teachers and eager students.</p></div>
+        <div><img src="" alt="" /></div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -1,59 +1,122 @@
-import React from 'react'
-import './Styles.css'
-import { NavLink } from 'react-router-dom'
-
-import Images from '../Images/Grammar-correction.svg'
-
-import Radiobtn from '../Components/RadioBtn/Radiobtn'
+import React, { useState } from "react";
+import "./Styles.css";
+import { NavLink } from "react-router-dom";
+import Images from "../Images/Grammar-correction.svg";
+import Radiobtn from "../Components/RadioBtn/Radiobtn";
 
 const Signup = () => {
+  // State to hold user input
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Prepare data object to send to the backend
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+
+    try {
+      // Send data to backend (you need to implement this part)
+      const response = await fetch("http://localhost:4400/api/student/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      // Handle response
+      if (response.ok) {
+        // Registration successful, you can redirect or do something else
+        console.log("Registration successful");
+      } else {
+        // Registration failed, handle accordingly
+        console.error("Registration failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className='section'>
-      <article className='article'>
-         <div className='logo-1'>
-            <img src="" alt="" />
-            <h4 className='logo-head'>logo</h4>
-         </div>
-         <div className='header'>
-           <h3 className='head'>WELCOME</h3>
-           <h4 className='Sub-head'>join us today !!</h4>
-         </div>
-
-         <div className='inpts'>
-          <form  action=''>
-
-          <input type="text"  className='input-x input-4' placeholder='Firstname'/>
-
-          <input type="text"  className='input-x input-5' placeholder='Lastname'/>
-
-          <input type="text"  className='input-x input-6' placeholder='Email'/>
-
-          <input type="text"  className='input-x input-7' placeholder='Password'/>
-
-          </form>
-         </div>
-         
-       <div className='rad-btns'>
-         <Radiobtn/>
-       </div>
-       
-       <div className="signupage">
-          <span>Already have an account? </span>
-          <NavLink to='/Login' style={{color:'green'}} className="link">
-            login
-          </NavLink>
+    <div className="section">
+      <article className="article">
+        <div className="logo-1">
+          <img src="" alt="" />
+          <h4 className="logo-head">logo</h4>
         </div>
-        <div className='btn-4'>Signup</div>
-       </article>
-       
-       {/* {rightsection} */}
+        <div className="header">
+          <h3 className="head">WELCOME</h3>
+          <h4 className="Sub-head">join us today !!</h4>
+        </div>
 
-       <div className="right-part">
-          <img src={Images} alt=""  className='imgs'/>
-       </div>
+        <div className="inpts">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="input-x input-4"
+              placeholder="Firstname"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
 
+            <input
+              type="text"
+              className="input-x input-5"
+              placeholder="Lastname"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+
+            <input
+              type="text"
+              className="input-x input-6"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+              type="text"
+              className="input-x input-7"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="rad-btns">
+              <Radiobtn />
+            </div>
+
+            <div className="signupage">
+              <span>Already have an account? </span>
+              <NavLink to="/Login" style={{ color: "green" }} className="link">
+                login
+              </NavLink>
+            </div>
+
+            <button type="submit" className="btn-4">
+              Signup
+            </button>
+          </form>
+        </div>
+      </article>
+
+      {/* {rightsection} */}
+
+      <div className="right-part">
+        <img src={Images} alt="" className="imgs" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
