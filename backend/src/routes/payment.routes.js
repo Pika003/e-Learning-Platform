b8b7529b21d3a3b1d778085e659408044e31ffcb
Parag <kadyanparag@gmail.com>
2024-04-22 19:13:33 +0530

    bug fix

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -9,7 +9,7 @@ router.route("/course/:courseID/:coursename").post(authSTD, coursePayment)
 
 router.route("/razorkey").get(authSTD, getkey)
 
-router.route("/course/:courseID/confirmation").post(authSTD, coursePaymentConfirmation)
+router.route("/course/confirmation/:coureID").post(authSTD, coursePaymentConfirmation)
 
 
 export default router;
